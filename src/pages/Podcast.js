5b7957a6236abb7c607e5e70495b9c94ec1dc857
Matt0107 <mathieu.durimel@gmail.com>
2024-11-09 10:51:36 +0100

    youtube cookies update

--- src/pages/Podcast.js
+++ src/pages/Podcast.js
@@ -66,9 +66,9 @@ const Podcast = () => {
             title="YouTube video player"
             width="100%"
             height="100%"
-            src={`https://www.youtube.com/embed/${selectedVideo}`}
+            src={`https://www.youtube-nocookie.com/embed/${selectedVideo}`}
             frameBorder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen
           ></iframe>
         )}
